70ec2910c61043e977b51edac9a8f008b18a00a5
amarkumarnew1234-cmyk <amarkumarnew1234@gmail.com>
2026-03-04 18:51:44 +0530

    Add files via upload

---
+++ slicing_concatenation.py
@@ -0,0 +1,9 @@
+import numpy as np
+data = np.array([1,2,3,4,5])
+print("print the whole matrix:",data)
+print("print the last two digit :",data[-2:])
+
+e = np.array([1,2,3])
+f = np.array([4,5,6])
+g = np.concatenate((e,f))
+print("g =",g)
